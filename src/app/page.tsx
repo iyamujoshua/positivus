1aa4f9324f4dbab1cfec8d13bb5203150a8f8e94
iyamujoshua <joshamu20@gmail.com>
2025-09-18 22:20:32 -0700

    pushed hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import NavBar from "@/Components/Navbar";
+import HeroSection from "@/Components/Herosection";
 export default function Home() {
   return (
    <>
 <NavBar />
+<HeroSection/>
    </>
   );
 }
